22f52ddb5e8dad1603876d3a324da7668b8722d3
Giancarlo A. Chiappe <gchiappe@linkfast.io>
2024-05-25 14:24:54 -0500

    added initial structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,30 @@
-use pgrx::pg_shmem_init;
+use pgrx::lwlock::PgLwLock;
+use pgrx::shmem::*;
 use pgrx::prelude::*;
+use pgrx::pg_shmem_init;
 
 pgrx::pg_module_magic!();
 
+
+const MAX_CALENDARS: usize = 128;
+const MAX_ENTRIES_PER_CALENDAR: usize = 8 * 1024;
+
+pub struct Calendar {
+    calendar_id: i64,
+    dates: heapless::Vec<i64, MAX_ENTRIES_PER_CALENDAR>
+}
+
+unsafe impl PGRXSharedMemory for Calendar {}
+
+static CALENDARS: PgLwLock<heapless::Vec<Calendar, MAX_CALENDARS>> = PgLwLock::new();
+static CALENDAR_ID_INDEX_MAP: PgLwLock<heapless::FnvIndexMap<i64, i64, MAX_CALENDARS>> = PgLwLock::new();
+static CALENDAR_XUID_INDEX_MAP: PgLwLock<heapless::FnvIndexMap<&'static str, i64, MAX_CALENDARS>> = PgLwLock::new();
+
 #[pg_guard]
 pub extern "C" fn _PG_init() {
+    pg_shmem_init!(CALENDARS);
+    pg_shmem_init!(CALENDAR_ID_INDEX_MAP);
+    pg_shmem_init!(CALENDAR_XUID_INDEX_MAP);
     unsafe {
         init_gucs();
     }
@@ -36,7 +56,7 @@ mod tests {
 
     #[pg_test]
     fn test_hello_kq_fx_calendar() {
-        assert_eq!("Hello, kq_fx_calendar", crate::hello_kq_fx_calendar());
+        assert_eq!("Hello, kq_imcx", crate::hello_kq_fx_calendar());
     }
 
 }
