cf67f09f5099096737b30dad81117310c70a71e9
Giancarlo A. Chiappe <gchiappe@linkfast.io>
2024-11-12 01:10:57 -0500

    updated update sql script

--- sql/kq_imcx--1.0.0--1.0.1.sql
+++ sql/kq_imcx--1.0.0--1.0.1.sql
@@ -1,3 +1,3 @@
 -- Empty upgrade script.
 
-RAISE NOTICE 'ketteQ In-Memory Calendar Extension Upgrade: kq_fx: 1.0.0 -> 1.0.1 completed.';
+DO $$ begin RAISE NOTICE 'ketteQ In-Memory Calendar Extension Upgrade: kq_fx: 1.0.0 -> 1.0.1 completed.'; end; $$;
